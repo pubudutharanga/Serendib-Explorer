da81e682451e794eac01abb8a7b9ce22ef859f24
Pubudu Tharanga <pubudutharange@gmail.com>
2025-11-04 15:13:41 +0530

    Deploy: 2025-11-04 15:13:41

--- scripts/create-404.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-try {
-  fs.copyFileSync('dist/index.html', 'dist/404.html');
-  console.log('Copied index.html to dist/404.html');
-} catch (err) {
-  console.error('Failed to create 404.html:', err);
-  process.exit(1);
-}
