2060d0f6ea4eefe9847e037702f5452a6cf2936c
Pubudu Tharanga <pubudutharange@gmail.com>
2025-11-04 17:35:09 +0530

    fix: set vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
+  base: './',
   plugins: [react(), imagetools()],
   server: { port: 5173 },
- base: "/Serendib-Explorer",
   }
 )
