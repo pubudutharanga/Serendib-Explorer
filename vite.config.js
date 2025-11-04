0a0af870efbd9b317648bdda7811c0c91c40c7c3
Pubudu Tharanga <pubudutharange@gmail.com>
2025-11-04 16:54:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,6 @@ import { imagetools } from 'vite-imagetools'
 export default defineConfig({
   plugins: [react(), imagetools()],
   server: { port: 5173 },
-   base: '/Serendib-Explorer',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.js',
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    // This helps with routing
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    }
+ base: "/Serendib-Explorer",
   }
-})
+)
